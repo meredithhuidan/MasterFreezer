7614543c2638cb06ee28f7adcd2523e023df94d6
meredithhuidan <1085771688@qq.com>
2018-04-30 21:51:43 +0800

    智能生鲜系统

---
+++ MasterFreezer/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MasterFreezer
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer ShowTimer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            // 在此点下面插入创建对象所需的代码。
+            //show timer by_songgp
+            ShowTimer = new System.Windows.Threading.DispatcherTimer();
+            ShowTimer.Tick += new EventHandler(ShowCurTimer);//起个Timer一直获取当前时间
+            ShowTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            ShowTimer.Start();
+        }
+        //show timer by_songgp
+        public void ShowCurTimer(object sender, EventArgs e)
+        {
+            //"星期"+DateTime.Now.DayOfWeek.ToString(("d"))
+
+            //获得星期几
+            this.Tt.Text = DateTime.Now.ToString("dddd", new System.Globalization.CultureInfo("zh-cn"));
+            this.Tt.Text += " ";
+            //获得年月日
+            this.Tt.Text += DateTime.Now.ToString("yyyy年MM月dd日");   //yyyy年MM月dd日
+            this.Tt.Text += " ";
+            //获得时分秒
+            this.Tt.Text += DateTime.Now.ToString("HH:mm");
+            //System.Diagnostics.Debug.Print("this.ShowCurrentTime {0}", this.ShowCurrentTime);
+        }
+        private void Btnjijian_Click(object sender, RoutedEventArgs e)
+        {
+            if (true)//里面掉验证函数
+            {
+                // 打开子窗体
+                CunfangWindow aChild = new CunfangWindow();
+                aChild.Show();
+                // 隐藏自己(父窗体)
+                this.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+        private void btnqujian_Click(object sender, RoutedEventArgs e)
+        {
+            if (true)//里面掉验证函数
+            {
+                // 打开子窗体
+                QujianWindow aChild = new QujianWindow();
+                aChild.Show();
+                // 隐藏自己(父窗体)
+                this.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void btnzancun_Click(object sender, RoutedEventArgs e)
+        {
+            if (true)//里面掉验证函数
+            {
+                // 打开子窗体
+                ZancunWindow aChild = new ZancunWindow();
+                aChild.Show();
+                // 隐藏自己(父窗体)
+                this.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void btnadministor_Click(object sender, RoutedEventArgs e)
+        {
+            if (true)//里面掉验证函数
+            {
+                // 打开子窗体
+                AdministorWindow aChild = new AdministorWindow();
+                aChild.Show();
+                // 隐藏自己(父窗体)
+                this.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ MasterFreezer/OpenWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MasterFreezer
+{
+    /// <summary>
+    /// OpenWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class OpenWindow : Window
+    {
+        private DispatcherTimer timer;
+        private ProcessCount processCount;
+        public OpenWindow()
+        {
+            InitializeComponent();
+        }
+          /// <summary>
+        /// 窗口加载事件
+         /// </summary>
+         /// <param name="sender"></param>
+        /// <param name="e"></param>
+         private void OpenWindow_Loaded(object sender, RoutedEventArgs e)
+         {
+             //设置定时器
+             timer = new DispatcherTimer();
+             timer.Interval = new TimeSpan(10000000);   //时间间隔为一秒
+             timer.Tick += new EventHandler(timer_Tick);
+
+             //转换成秒数
+             Int32 hour = Convert.ToInt32(HourArea.Text);
+             Int32 minute = Convert.ToInt32(MinuteArea.Text);
+             Int32 second = Convert.ToInt32(SecondArea.Text);
+
+             //处理倒计时的类
+             processCount = new ProcessCount(hour*3600+minute*60+second);
+             CountDown += new CountDownHandler(processCount.ProcessCountDown);
+ 
+             //开启定时器
+             timer.Start();
+         }       
+ 
+        /// <summary>
+        /// Timer触发的事件
+         /// </summary>
+         /// <param name="sender"></param>
+         /// <param name="e"></param>
+         private void timer_Tick(object sender, EventArgs e)
+         {
+             if (OnCountDown())
+             {
+                 HourArea.Text = processCount.GetHour();
+                 MinuteArea.Text = processCount.GetMinute();
+                 SecondArea.Text = processCount.GetSecond();
+             }
+             else
+                 timer.Stop();
+         }
+ 
+        /// <summary>
+         /// 处理事件
+         /// </summary>
+         public event CountDownHandler CountDown;
+         public bool OnCountDown()
+         {
+             if (CountDown != null)
+                return CountDown();
+ 
+             return false;
+         }
+
+        private class ProcessCount
+        {
+        }
+    }
+ 
+     /// <summary>
+     /// 处理倒计时的委托
+     /// </summary>
+    /// <returns></returns>
+     public delegate bool CountDownHandler();
+ }
+
+
+
+
